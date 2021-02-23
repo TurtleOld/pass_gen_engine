9eba900a31b1522eff38ecc3356e926fc70202f0
TurtleOld <shurpo.alexander@gmail.com>
2021-02-23 20:21:47 +0300

    Add installed_app - pass_generator and ROOT_URLCONF = pass_generator.urls

--- pass_gen_engine/settings.py
+++ pass_gen_engine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pass_generator',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pass_gen_engine.urls'
+ROOT_URLCONF = 'pass_generator.urls'
 
 TEMPLATES = [
     {
