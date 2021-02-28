2697d9f498ca41403fcf237c434228faa5f5033e
TurtleOld <shurpo.alexander@gmail.com>
2021-02-28 18:56:01 +0300

    Добавил функцию генерации пароля

--- pass_generator/views.py
+++ pass_generator/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+import random
+import string
 
 
 # Create your views here.
 
 def func(request):
     return render(request, 'pass_generator/index.html')
+
+
+def pass_generator():
+    random_pass = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(8)])
+    return random_pass
